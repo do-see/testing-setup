39bcaef29161d3d578f73915a08de5c8e31e8179
Tobias Schlitt <toby@php.net>
2016-07-27 12:03:02 +0200

    Implemented code to satisfy second test case.

--- src/main/Qafoo/Checkout.php
+++ src/main/Qafoo/Checkout.php
@@ -4,12 +4,20 @@ namespace Qafoo;
 
 class Checkout
 {
+    private $priceMap = array(
+        'B' => 23,
+        'D' => 42,
+    );
+
+    private $currentPrice = 0;
+
     public function scan($item)
     {
+        $this->currentPrice = $this->priceMap[$item];
     }
 
     public function getSum()
     {
-        return 23;
+        return $this->currentPrice;
     }
 }
