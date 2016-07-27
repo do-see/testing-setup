f45c02c50784ae7965a374c7327fde489c6a7009
Tobias Schlitt <toby@php.net>
2016-07-27 12:00:23 +0200

    Restructured test to visualize phases of a test.

--- test/Qafoo/CheckoutTest.php
+++ test/Qafoo/CheckoutTest.php
@@ -7,9 +7,10 @@ class CheckoutTest extends \PHPUnit_Framework_TestCase
     public function testScanSingleItem()
     {
         $checkout = new Checkout();
-        $checkout->scan('B');
-        $sum = $checkout->getSum();
 
+        $checkout->scan('B');
+
+        $sum = $checkout->getSum();
         $this->assertEquals(23, $sum);
     }
 }
